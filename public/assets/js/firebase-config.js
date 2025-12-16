838570efc8b7983305ce349327124ce594b82d8f
Agustin <agustinstella85@gmail.com>
2025-12-16 15:31:38 -0300

    cargar productos con foto

--- public/assets/js/firebase-config.js
+++ public/assets/js/firebase-config.js
@@ -5,10 +5,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyDZD4glBftOWVYdoviY-_v6Jzb6YD-QG38",
   authDomain: "nikiforrajeria.firebaseapp.com",
   projectId: "nikiforrajeria",
-  storageBucket: "nikiforrajeria.firebasestorage.app",
+  storageBucket: "nikiforrajeria.appspot.com",
   messagingSenderId: "98867618188",
   appId: "1:98867618188:web:4fa43e18ded6b38a8136a9",
   measurementId: "G-QPNNDNP1QD"
 };
+
 
 export default firebaseConfig;
